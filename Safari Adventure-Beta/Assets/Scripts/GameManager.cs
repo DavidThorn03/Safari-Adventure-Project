a60f7cce8f2d5a67acd96003f437b53d2206a1bb
DavidThorn03 <b00152842@mytudublin.ie>
2023-12-17 19:02:27 +0000

    Comments

--- Safari Adventure-Beta/Assets/Scripts/GameManager.cs
+++ Safari Adventure-Beta/Assets/Scripts/GameManager.cs
@@ -7,49 +7,55 @@ using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    public bool isGameActive = true;
-    public GameObject gameOver;
-    public TextMeshProUGUI scoreText;   
-    public int score;
-    public int playerSpeed = 5;
-    public GameObject enemyPrefab;
+    public bool isGameActive = true;//used in other scripts to see if game is running 
+    public int score = 0;
+    public int playerSpeed = 5;//speed of player/speed ground appears to be moving at
+
+    public TextMeshProUGUI scoreText;//text to show score
     public ParticleSystem dust;
 
-    // Start is called before the first frame update
+    public GameObject gameOver;//panel when game ends
+    public GameObject enemyPrefab;
+
     void Start()
     {
-        score = 0;
+
     }
 
-    // Update is called once per frame
     void Update()
     {
+        //if player pass enemy add one to score
         if(enemyPrefab.transform.position.z < -5)
         {
             UpdateScore(1);
         }
     }
+    //when game is over set is game active is false to running actions and show restart panel
     public void GameOver()
     {
         gameOver.SetActive(true);
         isGameActive = false;
         dust.Stop();
     }
+    //reset all variables and restart game(for buttons)
     public void RestartGame()
     {
         SceneManager.LoadScene(0);
         gameOver.SetActive(false);
         isGameActive = true;
     }
+    //transition to diffrent scene
     public void Home(int sceneID)
     {
         SceneManager.LoadScene(sceneID);
     }
+    //update score
     public void UpdateScore(int scoreToAdd)
     {
-        if (isGameActive){
+        if (isGameActive)
+        {
         score += scoreToAdd;
-        scoreText.text = "Score: " + score;
+        scoreText.text = "Score: " + score;//change score on screen
         }
     }
 }
--- Safari Adventure-Beta/Assets/Scripts/PlayerController.cs
+++ Safari Adventure-Beta/Assets/Scripts/PlayerController.cs
@@ -5,86 +5,107 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private float horizontalInput;
-    private float speed = 10.0f;
-    private float xRange = 9;
+    private float speed = 10.0f;//left to right player speed
+    private float xRange = 9;//furthest from 0 the player can move on the z axis (left to right)
     private float jumpForce = 600;
 
-    private bool StrPower = false;
-    private bool GunPower = false; 
+    private bool StrPower = false;//has strength power
+    private bool GunPower = false;//has gun power
     private bool isOnGround = false;
 
+    //player variables
     private Rigidbody playerRB;
-    private GameManager gm;
     private Animator playerAnim;
-    
+
+    //game manager script to use game manager methods and variables
+    private GameManager gm;
+
+    //powerup objects
     public GameObject powerupIndicator;
     public GameObject rocket;
 
+    //particles
     public ParticleSystem dust;
     public ParticleSystem explosion;
 
+    //player audios
     public AudioClip jumpSound;
     public AudioClip crashSound;
     public AudioSource playerAudio;
+
     void Start()
     {
+        //set components
         playerRB = GetComponent<Rigidbody>();
         gm = GameObject.Find("Game Manager").GetComponent<GameManager>();
         playerAnim = GetComponent<Animator>();
         playerAudio = GetComponent<AudioSource>();
-        dust.Play();
+        dust.Play();//play dust particles
     }
 
 
     void Update()
     {
-        if(transform.position.x < -xRange){
+        //if to far left stop moving
+        if(transform.position.x < -xRange)
+        {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
-        if(transform.position.x > xRange){
+        //if to far right stop moving
+        else if(transform.position.x > xRange)
+        {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
-        if(isOnGround && gm.isGameActive){
+        //if player is on ground and running make animation running and get left/right input
+        if(isOnGround && gm.isGameActive)
+        {
             playerAnim.SetBool("Jump_b", false);
             horizontalInput = Input.GetAxis("Horizontal");
             transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
         }
-        if(isOnGround && gm.isGameActive && Input.GetKeyDown(KeyCode.UpArrow)){
+        //if on the ground and up arrow is pressed jump with animation and play jump sound, stop dust animation
+        if(isOnGround && gm.isGameActive && Input.GetKeyDown(KeyCode.UpArrow))
+        {
             playerAnim.SetBool("Jump_b", true);
             playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
             playerAudio.PlayOneShot(jumpSound, 1.0f);
             dust.Stop();
         }
-        if(Input.GetKeyDown(KeyCode.Space) && GunPower){
+        //if the player has the gun powerup and presses space fire rocket
+        if(Input.GetKeyDown(KeyCode.Space) && GunPower)
+        {
             Instantiate(rocket, transform.position + new Vector3(0f, 1f, 1f), rocket.transform.rotation);
         }
-
+        //set postion and rotation if power up indicator
         powerupIndicator.transform.position = transform.position + new Vector3(0f, 0.5f, 0f);
         powerupIndicator.transform.Rotate(0f, 0.5f, 0f);
     }
 
 
     private void OnCollisionEnter(Collision collision){  
+        //if player hits strength power destroy the power up and activate the power
         if(collision.gameObject.CompareTag("StrPower")){
             Destroy(collision.gameObject);
             StrPowerMethod();
         }
-
+        //if player hits gun power destroy the power up and activate the power
         else if(collision.gameObject.CompareTag("GunPower")){
             Destroy(collision.gameObject);
             GunPowerMethod();
         }
-
+        //if player is on the ground set is on ground to true and play dust animation
         else if(collision.gameObject.CompareTag("Ground")){
             isOnGround = true;
             dust.Play();
         }
-
+        //if player hits an enemy...
         else if(collision.gameObject.CompareTag("Enemy")){
+            //and player doesnt have strength power end game, show game over screen and play death animation and sound
             if(!StrPower){
                 Debug.Log("GameOver");
                 gm.GameOver();
+                dust.Stop();
                 explosion.Play();
                 if(playerAnim.GetBool("Jump_b")){
                     playerAnim.SetInteger("DeathType_int", 2);
@@ -92,14 +113,14 @@ public class PlayerController : MonoBehaviour
                 playerAnim.SetBool("Death_b", true);
                 playerAudio.PlayOneShot(crashSound, 1.0f);
             }
+            //and if player does have strength powerup destroy the enemy and increment
             else{
                 Destroy(collision.gameObject);
                 gm.UpdateScore(1);
             }
-            dust.Stop();
         }
     }
-
+    //after power up is activated show powerup indicator and wait 5 seconds then deactivate
     IEnumerator PowerupCountdownRoutine()
     {
         powerupIndicator.gameObject.SetActive(true);
@@ -110,6 +131,7 @@ public class PlayerController : MonoBehaviour
         StrPower = false;
         GunPower = false;
     }
+    //activate strength power, deactivate gun power, increase player speed(animation and objects speed) and set power up timer
     private void StrPowerMethod(){
         StrPower = true;
         GunPower = false;
@@ -117,6 +139,7 @@ public class PlayerController : MonoBehaviour
         playerAnim.SetFloat("Speed_f", 1f);
         StartCoroutine(PowerupCountdownRoutine());
     }
+    //activate gun power, deactivate strength power and set power up timer
     private void GunPowerMethod(){
         StrPower = false;
         GunPower = true;
